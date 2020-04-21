6c92c9375c8b1434ba5456aabfd115cb09e56344
shevondevid <asvidanimeshpansilu98@gmail.com>
2020-04-22 03:22:59 +0530

    amanwella thama yko 2

--- app/src/main/java/com/example/mad_kavindu/activities/HomeScreen.java
+++ app/src/main/java/com/example/mad_kavindu/activities/HomeScreen.java
@@ -3,6 +3,7 @@ package com.example.mad_kavindu.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,11 +11,20 @@ import com.example.mad_kavindu.R;
 import com.example.mad_kavindu.adapters.CustomerListAdapter;
 
 public class HomeScreen extends AppCompatActivity {
-
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        btn1 =findViewById(R.id.admin_pb);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),admin_panel.class);
+                startActivity(i);
+            }
+        });
+
     }
 
     public void cutomerClick(View v) {
@@ -26,4 +36,5 @@ public class HomeScreen extends AppCompatActivity {
         Intent i=new Intent(this, Admin_PramotionList.class);
         startActivity(i);
     }
+
 }
